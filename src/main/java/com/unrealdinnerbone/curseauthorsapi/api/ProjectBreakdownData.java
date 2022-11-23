271217aa0b4765bc83647a698a343454410384d6
unrealdinnerbone@gmail.com <unrealdinnerbone@gmail.com>
2022-11-23 15:05:01 -0600

    Add Slug Method

--- src/main/java/com/unrealdinnerbone/curseauthorsapi/api/ProjectBreakdownData.java
+++ src/main/java/com/unrealdinnerbone/curseauthorsapi/api/ProjectBreakdownData.java
@@ -1,3 +1,9 @@
 package com.unrealdinnerbone.curseauthorsapi.api;
 
-public record ProjectBreakdownData(String projectName, String projectUrl, double points) {}
+public record ProjectBreakdownData(String projectName, String projectUrl, double points) {
+
+    public String getSlug() {
+        String[] split = projectUrl.split("/");
+        return split[split.length - 1];
+    }
+}
