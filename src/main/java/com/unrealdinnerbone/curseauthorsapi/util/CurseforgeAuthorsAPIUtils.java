12d9274ee820d84b509fbc9bd0eb3c029b72fd15
UnRealDinnerbone <unrealdinnerbone@gmail.com>
2023-09-14 10:39:57 -0500

    Try add more logging

--- src/main/java/com/unrealdinnerbone/curseauthorsapi/util/CurseforgeAuthorsAPIUtils.java
+++ src/main/java/com/unrealdinnerbone/curseauthorsapi/util/CurseforgeAuthorsAPIUtils.java
@@ -3,8 +3,12 @@ package com.unrealdinnerbone.curseauthorsapi.util;
 import com.unrealdinnerbone.unreallib.apiutils.APIUtils;
 import com.unrealdinnerbone.unreallib.apiutils.IResult;
 import com.unrealdinnerbone.unreallib.apiutils.ResponseData;
+import com.unrealdinnerbone.unreallib.exception.ExceptionFunction;
 import com.unrealdinnerbone.unreallib.json.JsonUtil;
+import com.unrealdinnerbone.unreallib.json.exception.JsonParseException;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.net.CookieManager;
 import java.net.HttpCookie;
@@ -13,6 +17,7 @@ import java.net.http.HttpClient;
 
 public class CurseforgeAuthorsAPIUtils {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CurseforgeAuthorsAPIUtils.class);
     private static final String BASE_URL = System.getenv().getOrDefault("AUTHORS_API_URL", "https://authors-next.curseforge.com" +
             "");
     private static final String COOKIE = System.getenv().getOrDefault("AUTHORS_COOKIE", "");
@@ -21,8 +26,19 @@ public class CurseforgeAuthorsAPIUtils {
 
     @NotNull
     public static <T> IResult<T> get(Class<T> tClass, String urlData) {
-        return APIUtils.getResult(CLIENT, tClass, BASE_URL + urlData, JsonUtil.DEFAULT)
-                .map(ResponseData::data);
+        IResult<ResponseData<T>> result = APIUtils.getResult(CLIENT, tClass, BASE_URL + urlData, JsonUtil.DEFAULT);
+        return result
+                .map(new ExceptionFunction<JsonParseException, ResponseData<T>, T>() {
+                    @Override
+                    public T apply(ResponseData<T> tResponseData) throws JsonParseException {
+                        if(tResponseData.statusCode() != 200) {
+                            JsonParseException jsonParseException = new JsonParseException("Not Vaild Response Code: " + tResponseData.statusCode());
+                            LOGGER.error("Not 200 " + tResponseData.data().toString(), tResponseData);
+                            throw jsonParseException;
+                        }
+                        return tResponseData.data();
+                    }
+                });
     }
 
 
